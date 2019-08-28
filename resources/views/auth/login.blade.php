d0af4de09904b113abdb521087c3d9ffd70a561e
Alfredo Ramírez Anastacio <xbox.07@hotmail.com>
2019-08-27 22:59:44 -0500

    Iniciando proyecto

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.form')
+
+@section('title', 'Inicio de Sesión')
+
+@section('subtitle', 'Ingresa tus datos para iniciar sesión')
+
+@section('content')
+    <div class="container mt--8 pb-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-7">
+                <div class="card bg-secondary shadow border-0">
+                    <div class="card-body px-lg-5 py-lg-5">
+                        @if ($errors->any())
+                            <div class="text-center text-muted mb-4">
+                                <small>Oops! Se encontró un error!</small>
+                            </div>
+                            <div class="alert alert-danger" role="alert">
+                                {{ $errors->first() }}
+                            </div>
+                        @endif
+
+
+
+
+                        <form role="form" method="POST" action="{{ route('login') }}">
+                        @csrf
+                            <div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="far fa-envelope"></i></span>
+						</div>
+						<input class="form-control" placeholder="Email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+					</div>
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-key"></i></span>
+                                    </div>
+                                    <input class="form-control" placeholder="Contraseña" type="password" name="password" required autocomplete="current-password">
+                                </div>
+                            </div>
+                            <div class="custom-control custom-control-alternative custom-checkbox">
+                                <input class="custom-control-input" id="remember" name="remember" type="checkbox" {{ old('remember') ? 'checked' : '' }}>
+                                <label class="custom-control-label" for="remember">
+                                <span class="text-muted">Recordar sesión</span>
+                                </label>
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary my-4">Ingresar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-6">
+                        <a href="{{ route('password.request') }}" class="text-light">
+                            <small>¿Olvidaste tu contraseña?</small>
+                        </a>
+                    </div>
+                    <div class="col-6 text-right">
+                        <a href="{{ route('register') }}" class="text-light">
+                            <small>¿Aún no te has registrado?</small>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
